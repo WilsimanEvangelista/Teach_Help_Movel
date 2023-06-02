ebc7202203607e1edb1d3517ac82dec167407706
20211tiimi0184 <hariandami@gmail.com>
2023-06-02 08:37:29 -0300

    tela de login criada

---
+++ app/src/main/java/davi/evelyn/harian/wilsiman/teachhelp/LoginActivity.java
@@ -0,0 +1,14 @@
+package davi.evelyn.harian.wilsiman.teachhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
